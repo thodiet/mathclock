32738d45b01de8cb35d4dd7409cb80a54e995567
thodiet <tdiet@web.de>
2026-04-12 20:29:46 +0200

    remove unused library

--- app/src/main/java/com/mathclock/MainActivity.kt
+++ app/src/main/java/com/mathclock/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Help
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
